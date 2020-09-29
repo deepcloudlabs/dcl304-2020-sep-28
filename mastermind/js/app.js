43bb00145f04c787cf1cc23783f7a06ddc1decbf
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-29 11:59:38 +0300

    implement controller full logic

--- mastermind/js/app.js
+++ mastermind/js/app.js
@@ -7,11 +7,37 @@ class MastermindController {
         this.playBtn = document.querySelector("#playBtn");
         this.movesBody = document.querySelector("#movesBody");
         this.playBtn.addEventListener('click', () => {
-            game.play(123);
-            console.log("Button is clicked!");
+            game.play(Number(this.inputTextGuess.value));
+            this.updateView();
         }, false);
     }
+
+    updateView = () => {
+        this.spanTries.innerText = this.game.tries;
+        this.spanGameLevel.innerText = this.game.gameLevel;
+        this.emptyElement(this.movesBody);
+        for (let move of this.game.moves) {
+            let tr = this.movesBody.insertRow();
+            let tdGuess = tr.insertCell(0);
+            let tdEvaluation = tr.insertCell(1);
+            tdGuess.appendChild(document.createTextNode(move.guess));
+            tdEvaluation.appendChild(document.createTextNode(move.evaluation));
+        }
+    }
+
+    emptyElement = (e) => {
+        let node = e;
+        while (e.hasChildNodes()) {
+            if (node.hasChildNodes()) {
+                node = node.lastChild;
+            } else {
+                node = node.parentNode;
+                node.removeChild(node.lastChild);
+            }
+        }
+    }
 }
+
 window.onload = () => {
     console.log("App is started...");
     let game = new Game(); // Model
