37f95f5268c9c61375580499aef91be24fac9227
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-30 16:27:53 +0300

    implement PUT, DELETE, PATCH

--- hr-backend/server.js
+++ hr-backend/server.js
@@ -11,14 +11,16 @@ import openApiDoc from "./swagger-hr-api.json";
  */
 const port = 9100;
 const api = express();
-
+const updatableFields = [
+    "salary", "photo", "department", "fulltime", "iban"
+];
 /*
     REST API Configuration
  */
 api.use(bodyParser.json({limit: "5mb"}));
 api.use(logger('dev'));
 // CORS Settings: Add a filter
-api.use(function(req,res,next){
+api.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "HEAD, POST, PUT, DELETE, PATCH, GET");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -100,7 +102,7 @@ const Employee = mongoose.model('employees', employeeSchema);
  */
 
 // Hiring an employee
-api.post("/hr/api/v1/employees", (req,res) => {
+api.post("/hr/api/v1/employees", (req, res) => {
     let emp = req.body;
     emp._id = emp.identityNo;
     let employee = new Employee(emp);
@@ -114,43 +116,87 @@ api.post("/hr/api/v1/employees", (req,res) => {
     });
 });
 
+const updateOrPatch = (req, res) => {
+    let emp = req.body;
+    let updatedEmp = {};
+    for (let field in emp) {
+        if (updatableFields.includes(field))
+            updatedEmp[field] = emp[field];
+    }
+    Employee.update(
+        {"identityNo": emp.identityNo},
+        {$set: updatedEmp},
+        {upsert: false},
+        (err, employee) => { // async
+            res.set("Content-Type", "application/json");
+            if (err) {
+                res.status(404).send({"status": err})
+            } else {
+                res.status(200).send({"status": "ok"});
+            }
+        }
+    );
+};
+
 // Updating an employee
-api.put("/hr/api/v1/employees/:identity", (req,res) => {
-
-});
+api.put("/hr/api/v1/employees", updateOrPatch);
 
 // Updating an employee
-api.patch("/hr/api/v1/employees/:identity", (req,res) => {
-
-});
+api.patch("/hr/api/v1/employees", updateOrPatch);
 
 // Firing the employee with the given identity
 // http delete http://localhost:9001/hr/api/v1/employees/11111111110
-api.delete("/hr/api/v1/employees/:identity", (req,res) => {
-
+api.delete("/hr/api/v1/employees/:identity", (req, res) => {
+    let identity = req.params.identity;
+    Employee.findOneAndDelete(
+        {'identityNo': identity},
+        {_id: false},
+        (err, emp) => { // async
+            res.set("Content-Type", "application/json");
+            if (err) {
+                res.status(404).send({"status": err})
+            } else {
+                res.status(200).send(emp);
+            }
+        }
+    );
 });
 
 // Querying the employee for the given identity
-api.get("/hr/api/v1/employees/:identity", (req,res) => {
-   let identity = req.params.identity;
-   Employee.findOne( // async
-       {"identityNo": identity},
-       {"_id": false},
-       (err,emp) => {
-           res.set("Content-Type", "application/json");
-           if (err) {
-               res.status(404).send({"status": "Not found"});
-           } else {
-               res.status(200).send(emp);
-           }
-       }
-   )
+api.get("/hr/api/v1/employees/:identity", async (req, res) => {
+    let identity = req.params.identity;
+    Employee.findOne( // async
+        {"identityNo": identity},
+        {"_id": false},
+        (err, emp) => {
+            res.set("Content-Type", "application/json");
+            if (err) {
+                res.status(404).send({"status": "Not found"});
+            } else {
+                res.status(200).send(emp);
+            }
+        }
+    )
 });
 
 // Querying the employees with pagination
 // http get http://localhost:9001/hr/api/v1/employees?page=10&size=15
-api.get("/hr/api/v1/employees", (req,res) => {
-
+api.get("/hr/api/v1/employees", (req, res) => {
+    let page = Number(req.query.page || 0);
+    let size = Number(req.query.size || 25);
+    let offset = page * size;
+    Employee.find(
+        {},
+        {_id: false},
+        {skip: offset, limit: size},
+        (err, employees) => {
+            if (err) {
+                res.status(404).send({"status": "Page not found"});
+            } else {
+                res.status(200).send(employees);
+            }
+        }
+    );
 });
 
 api.listen(port);
