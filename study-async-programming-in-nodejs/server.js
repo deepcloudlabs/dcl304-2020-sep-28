48e192f6fcc17c5587013dbc36a2ebb2ae681c1f
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-30 09:59:32 +0300

    install ws make a websocket connection to binance

--- study-async-programming-in-nodejs/server.js
+++ study-async-programming-in-nodejs/server.js
@@ -1,8 +1,19 @@
 let fetch = require("node-fetch");
+let WebSocket = require("ws");
 
+// async: setInterval -> event triggered (timeout event), fetch
 setInterval(() => {
+    // http
     fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
         .then(response => response.json())
         .then(ticker => console.log(ticker))
         .catch(err => console.error(err));
-}, 3000)
+}, 10000)
+
+// websocket -> HTML5 API -> (Near) Real-time Web Programming
+let ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
+ws.on('message', frame => {
+    let trade = JSON.parse(frame);
+    console.log(trade);
+})
+
