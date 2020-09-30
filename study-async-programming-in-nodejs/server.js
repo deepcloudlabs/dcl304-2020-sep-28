1c96f2417ef6507d4deafa810fe41b27cbb2e6f0
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-30 09:43:23 +0300

    install node-fetch make a rest call to binance ticker api using fetch api

--- study-async-programming-in-nodejs/server.js
+++ study-async-programming-in-nodejs/server.js
@@ -1,1 +1,8 @@
-console.log("Hello Mars!");
+let fetch = require("node-fetch");
+
+setInterval(() => {
+    fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+        .then(response => response.json())
+        .then(ticker => console.log(ticker))
+        .catch(err => console.error(err));
+}, 3000)
