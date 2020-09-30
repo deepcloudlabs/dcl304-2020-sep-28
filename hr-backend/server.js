a8ca75b3621b054462d5e5483bcc0b25386b172f
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-30 15:16:02 +0300

    add rest api skeleton

--- hr-backend/server.js
+++ hr-backend/server.js
@@ -1,6 +1,30 @@
 import mongoose from "mongoose";
 import utils from "./utils";
+import bodyParser from "body-parser";
+import logger from "morgan";
+import express from "express";
+/*
+    Creating REST Api using Express.js
+ */
+const port = 9100;
+const api = express();
 
+/*
+    REST API Configuration
+ */
+api.use(bodyParser.json({limit: "5mb"}));
+api.use(logger('dev'));
+// CORS Settings: Add a filter
+api.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "HEAD, POST, PUT, DELETE, PATCH, GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+/*
+     Persistence with MongoDB using Mongoose Library
+ */
 const mongodb_url = "mongodb://localhost:27017/hrdb";
 const mongo_opts = { // Dictionary
     "useNewUrlParser": true,
@@ -64,3 +88,42 @@ const employeeSchema = new mongoose.Schema({
 });
 
 const Employee = mongoose.model('employees', employeeSchema);
+
+/*
+     REST API DEFINITION
+ */
+
+// Hiring an employee
+api.post("/hr/api/v1/employees", (req,res) => {
+
+});
+
+// Updating an employee
+api.put("/hr/api/v1/employees/:identity", (req,res) => {
+
+});
+
+// Updating an employee
+api.patch("/hr/api/v1/employees/:identity", (req,res) => {
+
+});
+
+// Firing the employee with the given identity
+// http delete http://localhost:9001/hr/api/v1/employees/11111111110
+api.delete("/hr/api/v1/employees/:identity", (req,res) => {
+
+});
+
+// Querying the employee for the given identity
+api.get("/hr/api/v1/employees/:identity", (req,res) => {
+
+});
+
+// Querying the employees with pagination
+// http get http://localhost:9001/hr/api/v1/employees?page=10&size=15
+api.get("/hr/api/v1/employees", (req,res) => {
+
+});
+
+api.listen(port);
+console.log(`REST Api is up and running at port (${port}).`);
